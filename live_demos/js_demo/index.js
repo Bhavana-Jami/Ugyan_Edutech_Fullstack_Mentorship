bff9170f1c9d36e5de56f3871fb781a5105f32c6
Bhavana Jami <bhavanajami111@gmail.com>
2025-07-22 20:38:22 +0530

    js material added

---
+++ live_demos/js_demo/index.js
@@ -0,0 +1,8 @@
+// function square(number) {
+//   let result = number * number;
+//   return result;
+// }
+const listOfName = document.getElementsByClassName("name");
+console.log(listOfName[0]);
+// alert("you just have logged in")
+const value = prompt("enter a number")
