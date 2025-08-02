52d6687c1eac47ff141869442e35c14f2d62c872
Bhavana Jami <bhavanajami111@gmail.com>
2025-08-02 17:04:11 +0530

    added demos , material

--- live_demos/js_demo/index.js
+++ live_demos/js_demo/index.js
@@ -1,8 +1,4 @@
-// function square(number) {
-//   let result = number * number;
-//   return result;
-// }
-const listOfName = document.getElementsByClassName("name");
-console.log(listOfName[0]);
-// alert("you just have logged in")
-const value = prompt("enter a number")
+const nameId = document.getElementById("name")
+function handleClick(){
+    console.log(nameId.innerHTML)
+}
